35eb928c6f494401af9e4e170123f15388945943
Brianrmendes <mendesbrianr@gmail.com>
2021-10-13 19:09:21 +0530

    first commit

---
+++ action.js
@@ -0,0 +1,73 @@
+const videoCont = document.querySelector('.video-self');
+const mic = document.querySelector('#mic');
+const cam = document.querySelector('#webcam');
+
+
+//default media allowed
+let micAllowed = 1;
+let camAllowed = 1;
+
+
+//media setting as default
+let mediaConstraints = { video: true, audio: true };
+
+
+//used to stream video
+navigator.mediaDevices.getUserMedia(mediaConstraints)
+    .then(localstream => {
+        videoCont.srcObject = localstream;
+    })
+
+cam.addEventListener('click', () => {
+    if (camAllowed) {
+        mediaConstraints = { video: false, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "nodevice";
+        cam.innerHTML = `<i class="fas fa-video-slash"></i>`;
+        camAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: true, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "device";
+        cam.innerHTML = `<i class="fas fa-video"></i>`;
+        camAllowed = 1;
+    }
+})
+
+
+// mic initailization and on/off condition onclick
+mic.addEventListener('click', () => {
+    if (micAllowed) {
+        mediaConstraints = { video: camAllowed ? true : false, audio: false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.classList = "nodevice";
+        mic.innerHTML = `<i class="fas fa-microphone-slash"></i>`;
+        micAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: camAllowed ? true : false, audio: true };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.innerHTML = `<i class="fas fa-microphone"></i>`;
+        mic.classList = "device";
+        micAllowed = 1;
+    }
+})
+
+//
